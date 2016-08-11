9fd9c9b501c05e83ff7e3e552e657fb87c9f1757
Pedro Salgado <steenzout@ymail.com>
2016-08-11 08:06:03 -0600

    added bash script to build images.

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PG_MAJOR=9
+
+for PG_MINOR in 1 2 3 4 5 6
+do
+        VERSION="${PG_MAJOR}.${PG_MINOR}"
+
+	docker build \
+		-t "steenzout/postgres:${VERSION}" \
+		-f "${VERSION}/Dockerfile" \
+		"${VERSION}"
+done
+
